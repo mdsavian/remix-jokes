0e6be09def7a8498cacd7b905208cc77a6254027
Marlon Savian <marlon.savian@mercadofavo.com>
2022-04-05 10:17:58 -0300

    feat: show joke based on id

--- app/routes/jokes/$jokeId.tsx
+++ app/routes/jokes/$jokeId.tsx
@@ -1,15 +1,26 @@
+import { Link, useLoaderData } from "@remix-run/react";
+import { LoaderFunction } from "@remix-run/server-runtime";
+import { json } from "@remix-run/node";
 import { db } from "~/utils/db.server";
+import type { Joke } from "@prisma/client";
 
-type LoaderData = { joke: { name: string; content: string } };
+type LoaderData = { joke: Joke };
 
+export const loader: LoaderFunction = async ({ params }) => {
+  const joke = await db.joke.findUnique({ where: { id: params.jokeId } });
+
+  if (!joke) throw new Error("Joke not found");
+
+  const data: LoaderData = { joke };
+  return json(data);
+};
 export default function JokeRoute() {
+  const data = useLoaderData<LoaderData>();
   return (
     <div>
       <p>Here's your hilarious joke:</p>
-      <p>
-        Why don't you find hippopotamuses hiding in trees? They're really good
-        at it.
-      </p>
+      <p>{data.joke.content}</p>
+      <Link to=".">{data.joke.name} Permalink</Link>
     </div>
   );
 }
